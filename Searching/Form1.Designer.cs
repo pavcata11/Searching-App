11ff17fd8ef7369c36bb1fbdfb010f74ee10c073
Pavel Ivanchev <91673261+pavcata11@users.noreply.github.com>
2021-09-30 16:04:30 +0300

    Add files via upload

---
+++ Searching/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace Searching
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.firstNameTxtb = new System.Windows.Forms.TextBox();
+            this.secondNametxtb = new System.Windows.Forms.TextBox();
+            this.lastNameTxtb = new System.Windows.Forms.TextBox();
+            this.streetTxtb = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.productComboBox = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.searchTxtb = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchByToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientIdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.firstNameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lastNameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.productToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cityTxtb = new System.Windows.Forms.TextBox();
+            this.phoneTxtb = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.emailTxtb = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // firstNameTxtb
+            // 
+            resources.ApplyResources(this.firstNameTxtb, "firstNameTxtb");
+            this.firstNameTxtb.Name = "firstNameTxtb";
+            // 
+            // secondNametxtb
+            // 
+            resources.ApplyResources(this.secondNametxtb, "secondNametxtb");
+            this.secondNametxtb.Name = "secondNametxtb";
+            // 
+            // lastNameTxtb
+            // 
+            resources.ApplyResources(this.lastNameTxtb, "lastNameTxtb");
+            this.lastNameTxtb.Name = "lastNameTxtb";
+            // 
+            // streetTxtb
+            // 
+            resources.ApplyResources(this.streetTxtb, "streetTxtb");
+            this.streetTxtb.Name = "streetTxtb";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // productComboBox
+            // 
+            resources.ApplyResources(this.productComboBox, "productComboBox");
+            this.productComboBox.FormattingEnabled = true;
+            this.productComboBox.Items.AddRange(new object[] {
+            resources.GetString("productComboBox.Items"),
+            resources.GetString("productComboBox.Items1"),
+            resources.GetString("productComboBox.Items2"),
+            resources.GetString("productComboBox.Items3"),
+            resources.GetString("productComboBox.Items4"),
+            resources.GetString("productComboBox.Items5"),
+            resources.GetString("productComboBox.Items6"),
+            resources.GetString("productComboBox.Items7")});
+            this.productComboBox.Name = "productComboBox";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Linen;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.addBtn, "addBtn");
+            this.addBtn.Name = "addBtn";
+            this.addBtn.UseVisualStyleBackColor = false;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.deleteBtn, "deleteBtn");
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.UseVisualStyleBackColor = false;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // searchTxtb
+            // 
+            resources.ApplyResources(this.searchTxtb, "searchTxtb");
+            this.searchTxtb.Name = "searchTxtb";
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.searchBtn, "searchBtn");
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.UseVisualStyleBackColor = false;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.searchByToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            resources.ApplyResources(this.optionsToolStripMenuItem, "optionsToolStripMenuItem");
+            // 
+            // searchByToolStripMenuItem
+            // 
+            this.searchByToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientIdToolStripMenuItem,
+            this.firstNameToolStripMenuItem,
+            this.lastNameToolStripMenuItem,
+            this.productToolStripMenuItem});
+            this.searchByToolStripMenuItem.Name = "searchByToolStripMenuItem";
+            resources.ApplyResources(this.searchByToolStripMenuItem, "searchByToolStripMenuItem");
+            // 
+            // clientIdToolStripMenuItem
+            // 
+            this.clientIdToolStripMenuItem.Name = "clientIdToolStripMenuItem";
+            resources.ApplyResources(this.clientIdToolStripMenuItem, "clientIdToolStripMenuItem");
+            this.clientIdToolStripMenuItem.Click += new System.EventHandler(this.clientIdToolStripMenuItem_Click);
+            // 
+            // firstNameToolStripMenuItem
+            // 
+            this.firstNameToolStripMenuItem.Checked = true;
+            this.firstNameToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.firstNameToolStripMenuItem.Name = "firstNameToolStripMenuItem";
+            resources.ApplyResources(this.firstNameToolStripMenuItem, "firstNameToolStripMenuItem");
+            this.firstNameToolStripMenuItem.Click += new System.EventHandler(this.firstNameToolStripMenuItem_Click);
+            // 
+            // lastNameToolStripMenuItem
+            // 
+            this.lastNameToolStripMenuItem.Name = "lastNameToolStripMenuItem";
+            resources.ApplyResources(this.lastNameToolStripMenuItem, "lastNameToolStripMenuItem");
+            this.lastNameToolStripMenuItem.Click += new System.EventHandler(this.lastNameToolStripMenuItem_Click);
+            // 
+            // productToolStripMenuItem
+            // 
+            this.productToolStripMenuItem.Name = "productToolStripMenuItem";
+            resources.ApplyResources(this.productToolStripMenuItem, "productToolStripMenuItem");
+            this.productToolStripMenuItem.Click += new System.EventHandler(this.productToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            resources.ApplyResources(this.saveToolStripMenuItem, "saveToolStripMenuItem");
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            resources.ApplyResources(this.loadToolStripMenuItem, "loadToolStripMenuItem");
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // cityTxtb
+            // 
+            resources.ApplyResources(this.cityTxtb, "cityTxtb");
+            this.cityTxtb.Name = "cityTxtb";
+            // 
+            // phoneTxtb
+            // 
+            resources.ApplyResources(this.phoneTxtb, "phoneTxtb");
+            this.phoneTxtb.Name = "phoneTxtb";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // emailTxtb
+            // 
+            resources.ApplyResources(this.emailTxtb, "emailTxtb");
+            this.emailTxtb.Name = "emailTxtb";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cityTxtb);
+            this.groupBox1.Controls.Add(this.emailTxtb);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.addBtn);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.phoneTxtb);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.productComboBox);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.firstNameTxtb);
+            this.groupBox1.Controls.Add(this.secondNametxtb);
+            this.groupBox1.Controls.Add(this.streetTxtb);
+            this.groupBox1.Controls.Add(this.lastNameTxtb);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.searchTxtb);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataGridView1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox firstNameTxtb;
+        private System.Windows.Forms.TextBox secondNametxtb;
+        private System.Windows.Forms.TextBox lastNameTxtb;
+        private System.Windows.Forms.TextBox streetTxtb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox productComboBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.TextBox searchTxtb;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchByToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientIdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem firstNameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lastNameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem productToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox cityTxtb;
+        private System.Windows.Forms.TextBox phoneTxtb;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox emailTxtb;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Searching/Form1.cs
@@ -0,0 +1,221 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using WinSCP;
+
+
+namespace Searching
+{
+    public partial class Form1 : Form
+    {
+        List<Person> persons = new List<Person>();
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+           createPersonAddInList();
+           
+        }
+        private void createPersonAddInList()
+        {
+            var person = new Person()
+            {
+                ClientId = clientId(),
+                FirstName = firstNameTxtb.Text,
+                SecondName = secondNametxtb.Text,
+                LastName = lastNameTxtb.Text,
+                Phone = phoneTxtb.Text,
+                City = cityTxtb.Text,
+                Product = productComboBox.Text,
+                Street = streetTxtb.Text,
+                Email = emailTxtb.Text,
+            };
+            persons.Add(person);
+            clearFields();
+            addPersonInView();
+        }
+        private int clientId()
+        {
+            Random rand = new Random();
+                return  rand.Next(100000000);
+        }
+        private void clearFields()
+        {
+          
+            firstNameTxtb.Text = "";
+            secondNametxtb.Text = "";
+            lastNameTxtb.Text = "";
+            phoneTxtb.Text = "";
+            emailTxtb.Text = "";
+            cityTxtb.Text = "";
+            productComboBox.Text = "";
+            streetTxtb.Text = "";
+
+        }
+        private void addPersonInView()
+        {
+            dataGridView1.Rows.Clear();
+            createGridView();
+           
+            foreach (var person in persons)
+            {
+               
+                string[] row = new string[] { person.ClientId.ToString(), person.FirstName, person.SecondName, person.LastName, person.City, person.Street
+                ,person.Phone,person.Email,person.Product};
+                dataGridView1.Rows.Add(row);
+            }
+        }
+        private void createGridView()
+        {
+            dataGridView1.ColumnCount = 9;
+            dataGridView1.Columns[0].Name = "ClientID";
+            dataGridView1.Columns[1].Name = "First Name";
+            dataGridView1.Columns[2].Name = "Second Name";
+            dataGridView1.Columns[3].Name = "Last Name";
+            dataGridView1.Columns[4].Name = "City";
+            dataGridView1.Columns[5].Name = "Street";
+            dataGridView1.Columns[6].Name = "Phone";
+            dataGridView1.Columns[7].Name = "Email";
+            dataGridView1.Columns[8].Name = "Product";
+
+        }
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Refresh();
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.RemoveAt(item.Index);
+            }
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            string searchValue = searchTxtb.Text;
+            int rowIndex = -1;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["First Name"].Value != null) 
+                {
+                    string name = searchingBySelectedItemFromMenu();
+                    if (row.Cells[name].Value.ToString().Contains(searchValue))
+                    {
+                        rowIndex = row.Index;
+                        dataGridView1.Rows[rowIndex].DefaultCellStyle.BackColor = Color.FromArgb(255, 224, 192);
+                    }
+                    else
+                    {
+                        rowIndex = row.Index;
+                        dataGridView1.Rows[rowIndex].DefaultCellStyle.BackColor = Color.White;
+                    }
+                }
+            }
+        }
+        private string searchingBySelectedItemFromMenu()
+        {
+            if (clientIdToolStripMenuItem.Checked)
+            {
+                return "ClientID";
+            }
+            else if (firstNameToolStripMenuItem.Checked)
+            {
+                return "First Name";
+            }
+            else if (lastNameToolStripMenuItem.Checked)
+            {
+                return "Last Name";
+            }
+           
+                return "Product";
+        }
+        private void firstNameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clientIdToolStripMenuItem.Checked = false;
+            firstNameToolStripMenuItem.Checked = true;
+            lastNameToolStripMenuItem.Checked = false;
+            productToolStripMenuItem.Checked = false;
+
+        }
+
+        private void clientIdToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clientIdToolStripMenuItem.Checked = true;
+            firstNameToolStripMenuItem.Checked = false;
+            lastNameToolStripMenuItem.Checked = false;
+            productToolStripMenuItem.Checked = false;
+        }
+
+        private void lastNameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clientIdToolStripMenuItem.Checked = false;
+            firstNameToolStripMenuItem.Checked = false;
+            lastNameToolStripMenuItem.Checked = true;
+            productToolStripMenuItem.Checked = false;
+        }
+
+        private void productToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clientIdToolStripMenuItem.Checked = false;
+            firstNameToolStripMenuItem.Checked = false;
+            lastNameToolStripMenuItem.Checked = false;
+            productToolStripMenuItem.Checked = true;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string serializationFile = Path.Combine("myFile.txt");
+            using (Stream stream = File.Open(serializationFile, FileMode.Create))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                bformatter.Serialize(stream,persons);
+            }
+            SFTPFileTransfer.Send("myFile.txt");
+
+        }
+   
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SFTPFileTransfer.downloadFile();
+            loadDeserialization();
+            addPersonInView();
+        }
+        private void loadDeserialization()
+        {
+            using (Stream stream = File.Open("myFile.txt", FileMode.Open))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                persons = (List<Person>)bformatter.Deserialize(stream);
+            }
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+  }
+
